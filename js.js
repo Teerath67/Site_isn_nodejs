4051f59df78da3705160c802f16eaccf4632d4a7
Teerath <guillaume.girardi@viacesi.fr>
2019-05-19 14:07:21 +0200

    v1.1.0

--- js.js
+++ js.js
@@ -1,3 +1,62 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req, res) {
+  console.log('request was made : ' + req.url);
+  if(req.url == '/') {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf-8');
+    myReadStream.pipe(res);
+  } else if(req.url == '/creation_dun_compte.html') {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var myReadStream = fs.createReadStream(__dirname + '/creation_dun_compte.html', 'utf-8');
+    myReadStream.pipe(res);
+  }  else if(req.url == '/page_de_connexion.html') {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var myReadStream = fs.createReadStream(__dirname + '/page_de_connexion.html', 'utf-8');
+    myReadStream.pipe(res);
+  }   else if(req.url == '/page_de_connexion.html') {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var myReadStream = fs.createReadStream(__dirname + '/page_de_connexion.html', 'utf-8');
+    myReadStream.pipe(res);
+  }   else if(req.url == '/page_principale.html') {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var myReadStream = fs.createReadStream(__dirname + '/page_principale.html', 'utf-8');
+    myReadStream.pipe(res);
+  }   else if(req.url == '/questionnaire_1.html') {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var myReadStream = fs.createReadStream(__dirname + '/questionnaire_1.html', 'utf-8');
+    myReadStream.pipe(res);
+  } else if(req.url == '/style_main.css') {
+    res.writeHead(200, {'Content-Type': 'text/css'});
+    var myReadStream = fs.createReadStream(__dirname + '/style_main.css', 'utf-8');
+    myReadStream.pipe(res);
+  }  else if(req.url == '/style_creation_compte.css') {
+    res.writeHead(200, {'Content-Type': 'text/css'});
+    var myReadStream = fs.createReadStream(__dirname + '/style_creation_compte.css', 'utf-8');
+    myReadStream.pipe(res);
+  }  else if(req.url == '/style_page_connexion.css') {
+    res.writeHead(200, {'Content-Type': 'text/css'});
+    var myReadStream = fs.createReadStream(__dirname + '/style_page_connexion.css', 'utf-8');
+    myReadStream.pipe(res);
+  } else if(req.url == '/style_page_principale.css') {
+    res.writeHead(200, {'Content-Type': 'text/css'});
+    var myReadStream = fs.createReadStream(__dirname + '/style_page_principale.css', 'utf-8');
+    myReadStream.pipe(res);
+  } else if(req.url == '/style_questionnaire_1.css') {
+    res.writeHead(200, {'Content-Type': 'text/css'});
+    var myReadStream = fs.createReadStream(__dirname + '/style_questionnaire_1.css', 'utf-8');
+    myReadStream.pipe(res);
+  }
+
+
+});
+
+server.listen(3000, '127.0.0.1');
+console.log('listening port 3000');
+
+
+/*
 function submit_func() {
 
   var obj = {
@@ -11,12 +70,7 @@ function submit_func() {
   }
 
 
-
-
-
-
-
   var json = JSON.stringify(obj);
-  var fs = require('fs');
   fs.writeFile('myjsonfile.json', json, 'utf8', callback);
 }
+*/
